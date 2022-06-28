28eda19f85ea494a74b8a1f38773bb7f55fcc440
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-28 05:27:09 -0400

    Update screenshotter.py

--- screenshotter.py
+++ screenshotter.py
@@ -32,7 +32,7 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
 chrome_options = webdriver.ChromeOptions()
 options = [
    # "start-maximized",
-   "--window-size=10,10",
+   "--window-size=1000,1000",
     "--ignore-certificate-errors",
     "--hide-scrollbars",
     "--user-agent=[Mozilla/5.0 (Linux; Android 5.1; PULP Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36]"
