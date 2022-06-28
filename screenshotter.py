7593a47523e5f2019223de9a7812ce701e833246
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-28 05:17:13 -0400

    Update screenshotter.py

--- screenshotter.py
+++ screenshotter.py
@@ -20,7 +20,7 @@ PythonScriptPath = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 print(PythonScriptPath)
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(1200, 1200))
+display = Display(visible=0, size=(1920, 1024))
 display.start()
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
@@ -31,10 +31,11 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
 
 chrome_options = webdriver.ChromeOptions()
 options = [
-    "window-size=1200,1200",
-    "ignore-certificate-errors",
-    "hide-scrollbars"
-    "user-agent=[Mozilla/5.0 (Linux; Android 5.1; PULP Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36]"
+    "--start-maximized"
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors",
+    "--hide-scrollbars"
+    "--user-agent=[Mozilla/5.0 (Linux; Android 5.1; PULP Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36]"
 ]
 for option in options:
     chrome_options.add_argument(option)
