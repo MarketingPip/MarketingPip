6d27471caefeb55007d9e0262798473174221f9d
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-11 04:56:07 -0400

    Update screenshotter.py

--- screenshotter.py
+++ screenshotter.py
@@ -20,7 +20,7 @@ PythonScriptPath = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 print(PythonScriptPath)
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(1920, 500))
+display = Display(visible=0, size=(1920, 1024))
 display.start()
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
