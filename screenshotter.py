2f21bef481f0819c1acb60128630a07aaed87bf7
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-28 13:06:43 -0400

    Update screenshotter.py

--- screenshotter.py
+++ screenshotter.py
@@ -11,11 +11,13 @@ import time
 
 
 
+
+
 PythonScriptPath = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 print(PythonScriptPath)
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(1200, 1200))
+display = Display(visible=0, size=(800, 800))
 display.start()
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
@@ -26,10 +28,14 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
 
 chrome_options = webdriver.ChromeOptions()
 options = [
-    "--window-size=1200,1200",
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
     "--ignore-certificate-errors",
-    "--hide-scrollbars"
-    "user-agent=[Mozilla/5.0 (Linux; Android 5.1; PULP Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36]"
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    '--remote-debugging-port=9222'
 ]
 for option in options:
     chrome_options.add_argument(option)
@@ -46,9 +52,6 @@ input_variable3 = "File"
 input_variable4 = "MarketingPip"
 
 
-
-
-
 FileNames = input_variable
 
 
@@ -59,10 +62,9 @@ FileNames = input_variable
 if input_variable3 == "URL":
     Files = FileNames.split(',')
     ReplaceText = "https://"
-    Type="https://"
+    Type="http://"
     Type2 = ""
-    Type2 = Type2.replace(" ", "")
-    Sleep = 10
+    Sleep = 60
 
 
 
@@ -70,7 +72,7 @@ else:
 
     Files = FileNames.split('.')
     ReplaceText = ".html"
-    Type = f"file:///home/runner/work/{input_variable4}/{input_variable4}/"
+    Type = "file:///home/runner/work/Screenshot-Github-Action/Screenshot-Github-Action/"
     Type2 = ".html"
     Sleep = 5
     
@@ -111,20 +113,14 @@ for s in File_Names_List:
     else:
         pass
     ScreenshotPath = ScreenshotPath + ScreenshotName
-    ScreenshotPath = ScreenshotPath.replace("/", "-") 
     try:
         ScreenshotPath = ScreenshotPath.split('.com', 1)[0] + '.png'
         #driver.get('/home/runner/work/ProxyScraper-PY/ProxyScraper-PY/index.html')
-        #driver.get("https://marketingpipeline.github.io/Markdown-Tag")
         driver.get(Link)
-        driver.execute_script("document.querySelector('html').style.overflow = 'hidden';")
-        time.sleep(Sleep)
-      #  el = driver.find_element_by_tag_name('body')
-       # el = driver.save_screenshot(ScreenshotPath)
-        print("Screenshot captured")
-        print(Link)
+        print(link)
         print(ScreenshotPath)
+        time.sleep(Sleep)
+        T = driver.save_screenshot(ScreenshotPath)
+        print("Screenshot captured")
     except IOError as e:
         print(e)
-
-driver.close()
