97a1d4294e4de6f5850b7a6255c09e82dcd590f2
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-15 19:58:28 -0400

    Update and rename screenshotter.py to .github/screenshotter.py

--- .github/screenshotter.py
+++ .github/screenshotter.py
@@ -1,3 +1,5 @@
+# Made by github.com/MarketingPipeline
+# DO NOT REMOVE THIS NOTICE ABOVE. 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
