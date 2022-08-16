a84430feef9c01f12183846cbfbaf4bbef62bd85
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-15 20:11:12 -0400

    Added missing import

--- .github/screenshotter.py
+++ .github/screenshotter.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 import os
 import json
